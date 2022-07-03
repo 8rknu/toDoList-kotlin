355c55badc9f6a8d19a186a1043063a8d9aae7ea
8rknu <berkan.uygunucarlar@gmail.com>
2022-07-03 09:42:09 +0300

    Icon and splashscreen design is finished

---
+++ app/src/main/java/com/berkan_uygunucarlar/todolist/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.berkan_uygunucarlar.todolist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Thread.sleep(1000)
+        installSplashScreen()
+
+        setContentView(R.layout.activity_main)
+    }
+}
